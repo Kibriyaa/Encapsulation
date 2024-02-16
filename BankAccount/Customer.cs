6cb999a8d6a37ed7822781908aecf120d0f8bbd7
Kibriyaa <kibriya_ahmed@hotmail.co.uk>
2024-02-16 13:35:31 +0000

    Created the main program class and customer class which runs a program to display a customers first name and last name, which displays the name John Smith aswell as the second customer Gordan Ramsey

---
+++ BankAccount/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount {
+    public class Customer {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public Customer(string firstName, string lastName) {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using BankAccount;
+
+Customer customer = new Customer("John", "Smith");
+Customer customer2 = new Customer("Gordan", "Ramsey");
+
+Console.WriteLine($"{customer.FirstName} {customer.LastName}");
+Console.WriteLine($"{customer2.FirstName} {customer2.LastName}");
